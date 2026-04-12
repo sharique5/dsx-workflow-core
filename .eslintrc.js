92c249f8ff794c0748e54a98fffbcc1008d89318
Mohammed Sharique <hiresharique@gmail.com>
2026-04-12 12:14:08 +0530

    chore: init monorepo root (turbo, npm workspaces, eslint, prettier)

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    'no-console': ['warn', { allow: ['warn', 'error'] }],
+  },
+  ignorePatterns: ['dist/', 'node_modules/', '.turbo/', 'coverage/'],
+};
